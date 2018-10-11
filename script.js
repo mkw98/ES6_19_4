3d88b47ba578c7b9f95ce2d9086259963ae2e5cc
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-10-11 16:09:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 /*Zadanie pierwsze
 Połączenie dwóch stringów przy użyciu operatora + jest bardzo łatwym zadaniem. 
-Innym sposobem jest użycie metody concat albo join, ale co jeśli nie moglibyśmy skorzystać z żadnej z tych opcji? Twoim zadaniem będzie stworzenie dwóch zmiennych z wartościami Hello oraz World, a następnie połączenie ich metodą inną niż wymienione powyżej.
+Innym sposobem jest użycie metody concat albo join, ale co jeśli nie moglibyśmy skorzystać z żadnej z tych opcji?
+Twoim zadaniem będzie stworzenie dwóch zmiennych z wartościami Hello oraz World, 
+a następnie połączenie ich metodą inną niż wymienione powyżej. */
 
-Zadanie drugie
+const = 'Hello';
+const = 'World';
+console.log('${Hello} ${World}')
+
+/*Zadanie drugie
+
 Stwórz funkcję multiply, która ma zwracać wynik działania operacji mnożenia dwóch wartości a i b. Przykładowo:
 
 multiply(2, 5) // 10
@@ -10,9 +17,14 @@ multiply(6, 6) // 36
 Zadanie wydaje się być proste, ale co jeśli użytkownik poda na wejściu tylko jedną wartość? Przykładowo:
 
 multiply(5) // ?
-Chcemy, aby wynik takiego wywołania był również prawidłowy - możesz założyć, że jeśli użytkownik nie poda któregoś z parametrów, ma on zostać zastąpiony 1. Nie wolno korzystać z instrukcji warunkowych! Funkcję stwórz za pomocą arrow function.
+Chcemy, aby wynik takiego wywołania był również prawidłowy - możesz założyć, że jeśli użytkownik nie poda któregoś z parametrów,
+ma on zostać zastąpiony 1. Nie wolno korzystać z instrukcji warunkowych! Funkcję stwórz za pomocą arrow function.
+*/
 
-Zadanie trzecie
+multiply (a, b=1) => {a * b }
+console.log(multiply(5));
+
+/*Zadanie trzecie
 Napisz funkcję average, która obliczy średnią arytmetyczną wszystkich argumentów, które zostaną do niej przekazane. 
 Załóż, że argumenty zawsze będą liczbami:
 
@@ -20,11 +32,11 @@ average(1) // 1
 average(1, 3) // 2
 average(1, 3, 6, 6) // 4
 Skorzystaj z rest parameters! Funkcję stwórz za pomocą arrow function.
-
-Zadanie czwarte
+*/
+/*Zadanie czwarte
 Stwórz tablicę z ocenami const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1], a następnie w umiejętny sposób przekaż oceny do funkcji average tak, aby otrzymać wynik. Skorzystaj z operatora spread!
-
-Zadanie piąte
+*/
+/*Zadanie piąte
 Podczas pracy nad projektem natknąłeś się na bardzo dziwną strukturę danych - [1, 4, 'Iwona', false, 'Nowak']. 
 Twoim zadaniem jest skorzystanie z destrukturyzacji w celu wyciągnięcia z tablicy zmiennych firstname oraz lastname.
 
